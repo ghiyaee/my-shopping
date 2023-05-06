e0cb81e6691151f93582405ec9e9e287ee3e6567
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-05-06 15:31:10 +0430

    apdate footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,26 @@
+import { SiTelegram } from 'react-icons/si';
+import { BsWhatsapp } from 'react-icons/bs';
+import { AiTwotoneMail } from 'react-icons/ai';
+import { AiTwotonePhone } from 'react-icons/ai';
+import Link from 'next/link';
 const Footer = () => {
-  return <footer className="h-12 w-full shadow-inner border flex justify-center items-center text-2xl font-bold">footer</footer>;
+  return (
+    <footer
+      className="h-30 w-full shadow-inner border flex justify-around
+   items-center text-2xl font-bold flex-wrap p-8 gap-4"
+    >
+      <div>این یک وب سایت آزمایشی بوده و در حال توسعه می باشد</div>
+      <Link href={''}>
+        <div className="flex">
+          ارتباط با ما :&nbsp;
+          <SiTelegram className="text-blue-500 mr-5" />
+          <BsWhatsapp className="text-green-500 mr-5" />
+          <AiTwotoneMail className="mr-5" />
+          <AiTwotonePhone className="mr-5" />
+        </div>
+      </Link>
+    </footer>
+  );
 };
 
 export default Footer;
