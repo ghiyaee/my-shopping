ff3175df6df4c9976f6e51ac19c5d916aac30fe1
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-05-10 15:13:37 +0430

    updata meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,8 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, 
+          maximum-scale=1.0,minimum-scale=1.0, user-scalable=no"
         />
       </Head>
       <MyContextProvider>
