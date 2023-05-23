e9b1bb29c8abb60dc69ce14b5ec760e5a1e1e6ad
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-05-23 19:29:27 +0430

    fix style register

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,7 +1,7 @@
 import Register from "@/components/Register";
 const RegisterPage = () => {
     return (
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center gap-3">
         <Register />
       </div>
     );
