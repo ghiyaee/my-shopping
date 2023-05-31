9d9c2c8b69361593900f77f1f011eadec60d3c9d
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-05-31 18:06:23 +0430

    fix style mobile login

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import Header from "./Header"
 import Footer from "./Footer"
 function Layout({children}) {
   return (
-      <div className="flex justify-between flex-col min-h-screen gap-4">
+      <div className="flex justify-between flex-col min-h-screen">
           <Header/>
           {children}
           <Footer/>
